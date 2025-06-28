9eaad30010817454a1ed461e74066139624ae279
Lucas <soares0lucas8@gmail.com>
2025-06-28 19:30:43 -0300

    ajustes

--- Gateway/Gateway.Domain/Exceptions/HttpRequestFailGatewayException.cs
+++ Gateway/Gateway.Domain/Exceptions/HttpRequestFailGatewayException.cs
@@ -4,12 +4,9 @@ public class HttpRequestFailGatewayException : Exception
 {
     public readonly int StatusCode;
 
-    public string? Message { get; set; }
-
     public HttpRequestFailGatewayException(int statusCode, string message)
         : base(message)
     {
         StatusCode = statusCode;
-        Message = message;
     }
 }
