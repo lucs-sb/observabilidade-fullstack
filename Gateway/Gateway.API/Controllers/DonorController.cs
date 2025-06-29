ca1d75a291180cfe951be14c8e2edc49b27a15f8
Lucas <soares0lucas8@gmail.com>
2025-06-29 19:24:58 -0300

    feat: crud doador

---
+++ Gateway/Gateway.API/Controllers/DonorController.cs
@@ -0,0 +1,68 @@
+﻿using Gateway.API.Models.Donor;
+using Gateway.Domain.DTOs;
+using Gateway.Domain.DTOs.Response;
+using Gateway.Domain.Interfaces;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.API.Controllers;
+
+[ApiController]
+[Route("/api/donor")]
+[Authorize(Roles = "Admin")]
+public class DonorController : ControllerBase
+{
+    private readonly IDonorService _donorService;
+
+    public DonorController(IDonorService donorService)
+    {
+        _donorService = donorService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateDonorAsync([FromBody] DonorModel donorModel)
+    {
+        DonorDTO donorDTO = donorModel.Adapt<DonorDTO>();
+        await _donorService.CreateDonorAsync(donorDTO);
+        return Created();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDonorAsync([FromRoute] string id)
+    {
+        if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out _))
+            return BadRequest("Invalid donor ID.");
+
+        await _donorService.DeleteDonorAsync(Guid.Parse(id));
+        return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllDonorsAsync()
+    {
+        IEnumerable<List<DonorResponseDTO>> donors = await _donorService.GetAllDonorsAsync();
+        return Ok(donors);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetDonorByIdAsync([FromRoute] string id)
+    {
+        if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out _))
+            return BadRequest("Invalid donor ID.");
+
+        DonorResponseDTO donor = await _donorService.GetDonorByIdAsync(Guid.Parse(id));
+        return Ok(donor);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateDonorAsync([FromRoute] string id, [FromBody] DonorModel donorModel)
+    {
+        if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out _))
+            return BadRequest("Invalid donor ID.");
+
+        DonorDTO donorDTO = donorModel.Adapt<DonorDTO>();
+        await _donorService.UpdateDonorAsync(Guid.Parse(id), donorDTO);
+        return Accepted();
+    }
+}
