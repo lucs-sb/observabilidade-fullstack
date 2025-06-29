530b2eafbacdd690db57107d585ac4d837616768
Lucas <soares0lucas8@gmail.com>
2025-06-29 19:26:54 -0300

    feat: mapeamento doador

---
+++ Gateway/Gateway.API/Mappers/DonorMappingConfigurations.cs
@@ -0,0 +1,66 @@
+﻿using Gateway.API.Models.Donor;
+using Gateway.API.Models.Donor.Classes;
+using Gateway.Domain.DTOs;
+using Gateway.Domain.DTOs.Response;
+using Gateway.Infrastructure.Integrations.Message.Request.Donor;
+using Gateway.Infrastructure.Integrations.Message.Response.Donor;
+using Mapster;
+
+namespace Gateway.API.Mappers;
+
+public static class DonorMappingConfigurations
+{
+    public static void RegisterDonorMaps(this IServiceCollection services)
+    {
+        TypeAdapterConfig<DonorModel, DonorDTO>
+            .NewConfig()
+            .Map(dest => dest.FullName, src => src.FullName)
+            .Map(dest => dest.Email, src => src.Email)
+            .Map(dest => dest.PhoneNumber, src => src.PhoneNumber)
+            .Map(dest => dest.DateOfBirth, src => src.DateOfBirth)
+            .Map(dest => dest.Gender, src => src.Gender)
+            .Map(dest => dest.BloodType, src => src.BloodType)
+            .Map(dest => dest.Address, src => src.Address.Adapt<AddressDTO>());
+
+        TypeAdapterConfig<AddressModel, AddressDTO>
+            .NewConfig()
+            .Map(dest => dest.Street, src => src.Street)
+            .Map(dest => dest.City, src => src.City)
+            .Map(dest => dest.State, src => src.State)
+            .Map(dest => dest.ZipCode, src => src.ZipCode);
+
+        TypeAdapterConfig<DonorDTO, DonorRequest>
+            .NewConfig()
+            .Map(dest => dest.FullName, src => src.FullName)
+            .Map(dest => dest.Email, src => src.Email)
+            .Map(dest => dest.PhoneNumber, src => src.PhoneNumber)
+            .Map(dest => dest.DateOfBirth, src => src.DateOfBirth)
+            .Map(dest => dest.Gender, src => src.Gender)
+            .Map(dest => dest.BloodType, src => src.BloodType)
+            .Map(dest => dest.Address, src => src.Address.Adapt<AddressDTO>());
+
+        TypeAdapterConfig<AddressDTO, AddressRequest>
+            .NewConfig()
+            .Map(dest => dest.Street, src => src.Street)
+            .Map(dest => dest.City, src => src.City)
+            .Map(dest => dest.State, src => src.State)
+            .Map(dest => dest.ZipCode, src => src.ZipCode);
+
+        TypeAdapterConfig<DonorResponse, DonorResponseDTO>
+            .NewConfig()
+            .Map(dest => dest.FullName, src => src.FullName)
+            .Map(dest => dest.Email, src => src.Email)
+            .Map(dest => dest.PhoneNumber, src => src.PhoneNumber)
+            .Map(dest => dest.DateOfBirth, src => src.DateOfBirth)
+            .Map(dest => dest.Gender, src => src.Gender)
+            .Map(dest => dest.BloodType, src => src.BloodType)
+            .Map(dest => dest.Address, src => src.Address.Adapt<AddressDTO>());
+
+        TypeAdapterConfig<AddressResponse, AddressResponseDTO>
+            .NewConfig()
+            .Map(dest => dest.Street, src => src.Street)
+            .Map(dest => dest.City, src => src.City)
+            .Map(dest => dest.State, src => src.State)
+            .Map(dest => dest.ZipCode, src => src.ZipCode);
+    }
+}
--- Gateway/Gateway.API/Mappers/MappingConfigurations.cs
+++ Gateway/Gateway.API/Mappers/MappingConfigurations.cs
@@ -5,6 +5,7 @@ public static class MappingConfigurations
     public static IServiceCollection RegisterMaps(this IServiceCollection services)
     {
         services.RegisterAuthMaps();
+        services.RegisterDonorMaps();
 
         return services;
     }
