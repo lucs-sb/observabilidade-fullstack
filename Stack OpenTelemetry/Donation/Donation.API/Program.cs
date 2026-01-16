d42bf8febfb7a663e729f0804addba8c16de118f
Lucas Soares <soares0lucas8@gmail.com>
2026-01-16 18:25:27 -0300

    refactor: atualizando versão do projeto

--- Stack OpenTelemetry/Donation/Donation.API/Program.cs
+++ Stack OpenTelemetry/Donation/Donation.API/Program.cs
@@ -69,6 +69,12 @@ builder.Services.RegisterDonationMaps();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
---
+++ Stack OpenTelemetry/Donation/Donation.Infrastructure/Migrations/20260116212504_Migrations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Donation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_donation",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    donor_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    date_of_donation = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    donation_type = table.Column<int>(type: "integer", nullable: false),
+                    volume_ml = table.Column<int>(type: "integer", nullable: false),
+                    bag_number = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_donation", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_donation_bag_number",
+                table: "tb_donation",
+                column: "bag_number",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_donation");
+        }
+    }
+}
