2a099c1642b71391f7428c9f155cfe3390f04da2
Lucas Soares <soares0lucas8@gmail.com>
2026-01-16 19:13:17 -0300

    refactor: atualizando versão do projeto

---
+++ Stack ElasticSearch/Donor/Donor.Infrastructure/Migrations/20260116221301_Migrations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Donor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_donor",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    fullname = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    phonenumber = table.Column<string>(type: "text", nullable: true),
+                    dateofbirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    gender = table.Column<int>(type: "integer", nullable: false),
+                    bloodtype = table.Column<int>(type: "integer", nullable: false),
+                    rhfactor = table.Column<int>(type: "integer", nullable: false),
+                    weighthg = table.Column<decimal>(type: "numeric", nullable: true),
+                    street = table.Column<string>(type: "text", nullable: true),
+                    city = table.Column<string>(type: "text", nullable: true),
+                    state = table.Column<string>(type: "text", nullable: true),
+                    zipcode = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_donor", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_donor_email",
+                table: "tb_donor",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_donor");
+        }
+    }
+}
