eef39948d0612864f1c852692db9d814bb0cdf2e
Lucas Soares <soares0lucas8@gmail.com>
2026-01-18 02:55:59 -0300

    replicando alterações

--- Stack ElasticSearch/Donation/Donation.API/Program.cs
+++ Stack ElasticSearch/Donation/Donation.API/Program.cs
@@ -67,6 +67,12 @@ builder.Services.RegisterDonationMaps();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
